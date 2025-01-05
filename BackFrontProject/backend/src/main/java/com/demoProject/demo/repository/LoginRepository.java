7258ae0d827b2b21dca3d92efbdd56097d5beef6
Vikesh Chauhan <vikeshppk@gmail.com>
2025-01-05 12:13:41 +0530

    Remove Company code

--- BackFrontProject/backend/src/main/java/com/demoProject/demo/repository/LoginRepository.java
+++ BackFrontProject/backend/src/main/java/com/demoProject/demo/repository/LoginRepository.java
@@ -4,12 +4,9 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.demoProject.demo.entiry.Company;
 import com.demoProject.demo.entiry.Login;
 
 public interface LoginRepository extends JpaRepository<Login, Long>{
-
-	Company save(Company company);
 
 	List<Login> findByUsername(String email);
 
--- BackFrontProject/backend/src/main/java/com/demoProject/demo/service/LoginService.java
+++ BackFrontProject/backend/src/main/java/com/demoProject/demo/service/LoginService.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import com.demoProject.demo.entiry.Company;
 import com.demoProject.demo.entiry.Login;
 import com.demoProject.demo.repository.LoginRepository;
 
